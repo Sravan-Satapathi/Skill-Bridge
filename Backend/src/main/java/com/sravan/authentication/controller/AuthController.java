8a5e8666df12e3565b99e029c6131aa1c3a7b426
Sravan <ssaisravan2004@gmail.com>
2026-03-20 16:05:34 +0530

    resolve access restrictions

--- Backend/src/main/java/com/sravan/authentication/controller/AuthController.java
+++ Backend/src/main/java/com/sravan/authentication/controller/AuthController.java
@@ -50,7 +50,8 @@ public class AuthController {
                     .httpOnly(true)
                     .path("/")
                     .maxAge(Duration.ofDays(1))
-                    .sameSite("Strict")
+                    .sameSite("None")
+                    .secure(true)
                     .build();
             return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString())
                     .body(new AuthResponse(request.getEmail(), jwtToken));
